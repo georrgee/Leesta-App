57e1c92fe2059196c89ea9a86c69efeb870b8a49
George Garcia <geor@vicki-3.local>
2019-03-01 15:37:14 -0800

    Initial Commit

---
+++ Leesta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Leesta
+//
+//  Created by George Garcia on 3/1/19.
+//  Copyright © 2019 George Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
